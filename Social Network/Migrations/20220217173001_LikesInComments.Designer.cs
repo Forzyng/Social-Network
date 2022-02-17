573ea6f15cac79af76ef894be044f076005bdee8
Ture_gt79@student.itstep.org <Ture_gt79@student.itstep.org>
2022-02-17 19:31:13 +0200

    Added Likes in Comments

--- Social Network/Migrations/20220217173001_LikesInComments.Designer.cs
+++ Social Network/Migrations/20220217173001_LikesInComments.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Social_Network.Data;
 
 namespace Social_Network.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220217173001_LikesInComments")]
+    partial class LikesInComments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Social Network/Migrations/20220217173001_LikesInComments.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Social_Network.Migrations
+{
+    public partial class LikesInComments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CommentId",
+                table: "Likes",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_CommentId",
+                table: "Likes",
+                column: "CommentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Comments_CommentId",
+                table: "Likes",
+                column: "CommentId",
+                principalTable: "Comments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Comments_CommentId",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_CommentId",
+                table: "Likes");
+
+            migrationBuilder.DropColumn(
+                name: "CommentId",
+                table: "Likes");
+        }
+    }
+}
